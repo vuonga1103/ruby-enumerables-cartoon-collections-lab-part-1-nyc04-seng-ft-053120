e343774055144d812586a61381d19fa003c7b941
Anh Vuong <vuonga1103@yahoo.com>
2020-06-08 19:44:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
 def greet_characters(array)
+<<<<<<< HEAD
   array.each { |elem| puts "Hello #{elem}!" }
+=======
+  array.each { |elem| puts "Hello #{elem}" }
+>>>>>>> 3d1096426a54e413bf1286fa3c4cc74914c1576f
 end
 
 def list_dwarves(array)
